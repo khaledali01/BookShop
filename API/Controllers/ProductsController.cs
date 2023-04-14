f5027238f2db2cae611f8376e47edb72a9c09f7f
Khaled <khaled.ali.architect@gmail>
2023-04-14 14:20:05 +0200

    Implementing Repository Pattern For Database Access
    See: #2

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,5 +1,5 @@
 using Core.Entities;
-using Infrastructure.Data;
+using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,17 +10,17 @@ namespace API.Controllers
 
     public class ProductsController : ControllerBase
     {
-		private readonly StoreContext context;
+		private readonly IProductRepository repo;
 
-		public ProductsController(StoreContext context)
+		public ProductsController(IProductRepository repo)
 		{
-			this.context = context;
+			this.repo = repo;
 		}
 
 		[HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await context.Products.ToListAsync();
+            var products = await repo.GetProductsAsync();
 
             return Ok(products);
         }
@@ -28,8 +28,8 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            
-            return await context.Products.FindAsync(id);
+
+            return await repo.GetProductByIdAsync(id);
 		}
 
 
--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,11 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
