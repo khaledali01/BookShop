5e3f7e3e10d354383a177778365068d20f2d5e0c
Khaled <khaled.ali.architect@gmail>
2023-04-23 03:38:44 +0200

    API Pagination
    See: #18

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 using API.Dtos;
 using API.Errors;
+using API.Helpers;
 using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
@@ -29,15 +30,25 @@ namespace API.Controllers
 		}
 
 		[HttpGet]
-		public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(
-			string? sort, int? brandId, int? typeId)
+		public async Task<ActionResult<Pagination<ProductToReturnDto>>> GetProducts(
+			[FromQuery] ProductSpecParams productParams)
 		{
-			var spec = new ProductsWithTypesAndBrandsSpecification(sort, brandId, typeId);
+			var spec = new ProductsWithTypesAndBrandsSpecification(productParams);
+
+			var countSpec = new ProductWithFiltersForCountSpecification(productParams);
+
+			var totalItems = await productsRepo.CountAsync(countSpec);
 
 			var products = await productsRepo.ListAsync(spec);
 
-			return Ok(mapper
-				.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
+			var data = mapper
+				.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products);
+
+			return Ok(new Pagination<ProductToReturnDto>(productParams.PageIndex, 
+				productParams.PageSize,
+				totalItems,
+				data
+				));
 		}
 
 		[HttpGet("{id}")]
