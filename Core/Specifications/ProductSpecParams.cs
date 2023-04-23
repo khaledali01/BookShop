5f3189eac5cca3d4281407457b8a97d8c183cd78
Khaled <khaled.ali.architect@gmail>
2023-04-23 04:47:55 +0200

    API Search Functionality Implementation
    See: #19

--- Core/Specifications/ProductSpecParams.cs
+++ Core/Specifications/ProductSpecParams.cs
@@ -19,5 +19,13 @@
         public int? TypeId { get; set; }
 
         public string? Sort { get; set; }
+
+		private string search;
+
+        public string Search 
+		{
+			get => search;
+			set => search = value.ToLower();
+		}
     }
 }
