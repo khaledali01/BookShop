0959ecbf67b927b8b73d164dc98175427cc96a3c
Khaled <khaled.ali.architect@gmail>
2023-04-26 00:33:10 +0200

    Creating Product Interfaces in Angular Project
    See: #24

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from './models/product';
+import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +11,13 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   
   title = 'My Book Store';
-  products : any[];
+  products : IProduct[];
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.http.get('https://localhost:7155/api/products?pageSize=50')
-      .subscribe((response: any) => {
+      .subscribe((response: IPagination) => {
         this.products = response.data;
     }, error => {
       console.log(error);
