135aa76e99facd9658c372b4177c5066a632512f
Khaled <khaled.ali.architect@gmail>
2023-06-01 03:09:45 +0300

    API Basket
    See: #38

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -5,32 +5,33 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Extensions
 {
-	public static class ApplicationServicesExtensions
-	{
-		public static IServiceCollection AddApplicationServices(this IServiceCollection Services)
-		{
-			Services.AddScoped<IProductRepository, ProductRepository>();
-			Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+    public static class ApplicationServicesExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection Services)
+        {
+            Services.AddScoped<IProductRepository, ProductRepository>();
+            Services.AddScoped<IBasketRepository, BasketRepository>();
+            Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
-			Services.Configure<ApiBehaviorOptions>(options =>
-			{
-				options.InvalidModelStateResponseFactory = actionContext =>
-				{
-					var errors = actionContext.ModelState
-						.Where(e => e.Value.Errors.Count > 0)
-						.SelectMany(x => x.Value.Errors)
-						.Select(x => x.ErrorMessage).ToArray();
+            Services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = actionContext =>
+                {
+                    var errors = actionContext.ModelState
+                        .Where(e => e.Value.Errors.Count > 0)
+                        .SelectMany(x => x.Value.Errors)
+                        .Select(x => x.ErrorMessage).ToArray();
 
-					var errorResponse = new ApiValidation
-					{
-						Errors = errors
-					};
+                    var errorResponse = new ApiValidation
+                    {
+                        Errors = errors
+                    };
 
-					return new BadRequestObjectResult(errorResponse);
-				};
-			});
+                    return new BadRequestObjectResult(errorResponse);
+                };
+            });
 
-			return Services;
-		}
-	}
+            return Services;
+        }
+    }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -3,6 +3,7 @@ using API.Helpers;
 using API.Middleware;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,12 +18,17 @@ builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddDbContext<StoreContext>(x =>
  x.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddSingleton<IConnectionMultiplexer>(c =>
+           {
+               var configuration = ConfigurationOptions.Parse(builder.Configuration.GetConnectionString("Redis"), true);
+               return ConnectionMultiplexer.Connect(configuration);
+           });
 builder.Services.AddCors(opt =>
 {
-	opt.AddPolicy("CorsPolicy", policy =>
-	{
-		policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
-	});
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+    });
 });
 var app = builder.Build();
 
@@ -44,25 +50,25 @@ app.UseAuthorization();
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
-	c.SwaggerEndpoint("/swagger/v1/swagger.json", "BookStore API v1");
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "BookStore API v1");
 });
 
 app.MapControllers();
 
 using (var scope = app.Services.CreateScope())
 {
-	var services = scope.ServiceProvider;
-	var loggerFactory = services.GetRequiredService<ILoggerFactory>();
-	try
-	{
-		var context = services.GetRequiredService<StoreContext>();
-		await context.Database.MigrateAsync();
-		await StoreContextSeed.SeedAsync(context, loggerFactory);
-	}
-	catch (Exception ex)
-	{
-		var logger = loggerFactory.CreateLogger<Program>();
-		logger.LogError(ex, "An Error occured during migration");
-	}
-	app.Run();
+    var services = scope.ServiceProvider;
+    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+    try
+    {
+        var context = services.GetRequiredService<StoreContext>();
+        await context.Database.MigrateAsync();
+        await StoreContextSeed.SeedAsync(context, loggerFactory);
+    }
+    catch (Exception ex)
+    {
+        var logger = loggerFactory.CreateLogger<Program>();
+        logger.LogError(ex, "An Error occured during migration");
+    }
+    app.Run();
 }
